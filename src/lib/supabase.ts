d8b949851af5f4c353a0c6b1a97af1a61421bfb6
Vince77361 <130560043+Vince77361@users.noreply.github.com>
2026-03-25 13:13:18 +0900

    route fix

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
+// for client
+
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
